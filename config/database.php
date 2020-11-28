bf01b992b468a9a1e90126d3311ca5d1f0244cdf
emirsagit <emirsagit@gmail.com>
2020-11-28 14:13:22 +0300

    update db connection

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL= parse_url('postgres://ssbhzqszdgxyzz:1891999d026212c20e0d21670ddbb125cc11a51e0930855d6b6c8d10e2cbfcd7@ec2-54-147-126-202.compute-1.amazonaws.com:5432/d5m8db94dpilaj');
+
 return [
 
     /*
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,7 +70,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => ltrim($DATABASE_URL['path'], '/'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
